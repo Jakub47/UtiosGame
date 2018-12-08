44f2d0d45ffc422b3380baf9cfab6718567416f8
Jakub47 <jakub7447@gmail.com>
2018-12-08 02:47:11 +0100

    Insert model of house + create animation + raycast!

---
+++ Utios/Assets/Scripts/DoorManage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManage : MonoBehaviour 
+{
+	bool doorIsOpen;
+	float doorTimer = 0.0f;
+	
+	public float doorOpenTime = 10.0f;
+	public AudioClip doorOpenSound;
+	public AudioClip doorShutSound;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(doorIsOpen)
+		{
+			doorTimer += Time.deltaTime;
+		}
+		
+		if (doorTimer >= doorOpenTime) 
+		{
+			Door(doorShutSound , false , "CloseDoor");
+			doorTimer = 0.0f;
+		}
+	}
+
+	public void DoorCheck()
+	{
+		if (!doorIsOpen) 
+			Door (doorOpenSound, true , "OpenDoor");
+		else if(doorIsOpen)
+		{
+			if (!transform.GetComponent<Animation> ().IsPlaying("OpenDoor"))
+			{
+			Door(doorShutSound , false , "CloseDoor");
+			doorTimer = 0.0f;
+			}		
+		}
+	}
+
+	void Door(AudioClip aClip, bool OpenCheck, string animName  )
+	{
+		gameObject.GetComponent<AudioSource> ().PlayOneShot (aClip);
+		doorIsOpen = OpenCheck;
+		transform.GetComponent<Animation> ().Play (animName);
+	}
+	
+}
---
+++ Utios/Assets/Scripts/PlayerRCasting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerRCasting : MonoBehaviour 
+{
+	public GameObject ActionDisplay;
+	public GameObject ActionText;
+	public GameObject ExtraCursor;
+
+	void Update () 
+	{
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),
+						 out hit , 3f))
+		{		
+			if(hit.collider.gameObject.tag == "playerDoor")
+			{
+				ActionDisplay.SetActive(true);
+				ActionText.SetActive(true);
+				ExtraCursor.SetActive(true);
+				if(Input.GetButtonDown("Confirm"))
+					hit.collider.gameObject.GetComponent<DoorManage>().DoorCheck();
+			}
+			else
+			{
+				ActionDisplay.SetActive(false);
+				ActionText.SetActive(false);
+				ExtraCursor.SetActive(false);
+				
+			}
+		}
+	}
+}
