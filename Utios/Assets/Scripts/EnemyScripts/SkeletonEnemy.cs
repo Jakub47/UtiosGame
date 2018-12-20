03078b302b29ff1c7bebcbf434f2844a926e15ad
Jakub47 <jakub7447@gmail.com>
2018-12-20 16:14:44 +0100

    First Enemy with animations working [attact,idle,moving,death]

---
+++ Utios/Assets/Scripts/EnemyScripts/SkeletonEnemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonEnemy : MonoBehaviour
+{
+	public int EnemyHealth = 10;
+	public Animator anim;
+
+	void DeductPoints(int DamageAmount)
+	{
+		EnemyHealth -= DamageAmount;
+	}
+
+	//If the enemy health is 0 destroy
+	void Update () 
+	{
+		if (EnemyHealth <= 0) 
+		{
+			StartCoroutine(DeathSkeleton());
+			anim.gameObject.GetComponent<chase>().Dead();
+
+		}
+	}
+
+	IEnumerator DeathSkeleton()
+	{
+		GlobalEXP.CurrentExp += 10;
+		anim.SetBool ("isIdle", false);
+		anim.SetBool ("isWalking", false);
+		anim.SetBool ("isAttacking", false);
+		anim.SetBool ("isDead", true);
+		yield return new WaitForSeconds(2f);
+		Destroy(gameObject);
+
+	}
+}
---
+++ Utios/Assets/Scripts/Ezpierence/globalexpp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalexpp : MonoBehaviour {
+
+	public static int CurrentXp;
+	public int InternalXp;
+
+
+	// Use this for initialization
+	void Start () {
+		InternalXp = CurrentXp;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
